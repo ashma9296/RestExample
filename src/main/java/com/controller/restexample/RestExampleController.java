8fa61a008489c1b9bbfe70891c22e65a7611d819
ashma9296 <ashma.khadka2019@gmail.com>
2019-12-04 16:05:33 -0500

    application run

--- src/main/java/com/controller/restexample/RestExampleController.java
+++ src/main/java/com/controller/restexample/RestExampleController.java
@@ -1,5 +1,26 @@
 package com.controller.restexample;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restexampledto.DtoforRestexample;
+
+@RestController
+@RequestMapping("/service")
 public class RestExampleController {
+	
+	@GetMapping(value ="/provide")
+	public DtoforRestexample runProgrammer() {
+		DtoforRestexample dt = new DtoforRestexample();
+		
+		dt.setId(101);
+		dt.setName("Airengine");
+		dt.setCategory("infrastructure");
+		System.out.println(dt.toString());
+		return dt;
+		
+	}
 
 }
---
+++ src/main/java/com/service/restexample/RestExampleService.java
@@ -0,0 +1,5 @@
+package com.service.restexample;
+
+public class RestExampleService {
+
+}
